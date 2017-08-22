6eef0f39ff1383861f7f57176a8a8074029135c9
Nicholas Mayne <nmayne@gmail.com>
2017-08-21 23:02:22 -0700

    initial checkin

---
+++ Day13-Knights/GuestNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_Knights
+{
+    class GuestNode
+    {
+        string Name { get; set; }
+
+        Dictionary<string, int> happiness { get; set; }
+    }
+}
---
+++ Day13-Knights/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_Knights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = LoadData("test.txt");
+        }
+
+        private List<GuestNode> LoadData(string path)
+        {
+
+        }
+    }
+}
