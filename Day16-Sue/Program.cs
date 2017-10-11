d7a937a6deb7827cdb43d482ebc79470bd2a12b1
Nicholas Mayne <nmayne@gmail.com>
2017-10-11 00:55:04 -0700

    first part of Day16

---
+++ Day16-Sue/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Day16_Sue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aunts = LoadData("input.txt");
+
+            var masterData = new SueData
+            {
+                children = 3,
+                cats = 7,
+                samoyeds = 2,
+                pomeranians = 3,
+                akitas = 0,
+                vizslas = 0,
+                goldfish = 5,
+                trees = 3,
+                cars = 2,
+                perfumes = 1,
+            };
+
+            foreach(var aunt in aunts)
+            {
+                if(masterData.compare(aunt.Value))
+                {
+                    Console.WriteLine($"I found the aunt, it's number {aunt.Key}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<int, SueData> LoadData(string path)
+        {
+            var sues = new Dictionary<int, SueData>();
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    var nameSplit = s.Split(':');
+                    var sueNumber = int.Parse(nameSplit[0].Split(' ')[1]);
+
+                    var list = s.Substring(nameSplit[0].Length + 2);
+                    var listSplit = list.Split(',');
+
+                    var newSue = new SueData();
+
+                    foreach (var item in listSplit)
+                    {
+                        string itemName = item.Split(':')[0].Trim(' ');
+                        int itemValue = int.Parse(item.Split(':')[1]);
+
+                        Type type = newSue.GetType();
+                        PropertyInfo prop = type.GetProperty(itemName);
+                        prop.SetValue(newSue, itemValue, null);
+                    }
+
+                    sues[sueNumber] = newSue;
+                }
+            }
+
+            return sues;
+        }
+    }
+}
---
+++ Day16-Sue/SueData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_Sue
+{
+    public class SueData
+    {
+        public int children { get; set; }
+
+        public int cats { get; set; }
+
+        public int samoyeds { get; set; }
+
+        public int pomeranians { get; set; }
+
+        public int akitas { get; set; }
+
+        public int vizslas { get; set; }
+
+        public int goldfish { get; set; }
+
+        public int trees { get; set; }
+
+        public int cars { get; set; }
+
+        public int perfumes { get; set; }
+
+        public bool compare (SueData comp)
+        {
+            if(comp.children != 0 && comp.children != children)
+            {
+                return false;
+            }
+            
+            if (comp.cats != 0 && comp.cats != cats)
+            {
+                return false;
+            }
+
+            if (comp.samoyeds != 0 && comp.samoyeds != samoyeds)
+            {
+                return false;
+            }
+
+            if (comp.pomeranians != 0 && comp.pomeranians != pomeranians)
+            {
+                return false;
+            }
+
+            if (comp.akitas != 0 && comp.akitas != akitas)
+            {
+                return false;
+            }
+
+            if (comp.vizslas != 0 && comp.vizslas != vizslas)
+            {
+                return false;
+            }
+
+            if (comp.goldfish != 0 && comp.goldfish != goldfish)
+            {
+                return false;
+            }
+
+            if (comp.trees != 0 && comp.trees != trees)
+            {
+                return false;
+            }
+
+            if (comp.cars != 0 && comp.cars != cars)
+            {
+                return false;
+            }
+
+            if (comp.perfumes != 0 && comp.perfumes != perfumes)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
