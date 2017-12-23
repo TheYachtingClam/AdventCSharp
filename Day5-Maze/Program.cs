a7d6935b56db26b59d922a21d4a234420802091a
Nicholas Mayne <nmayne@gmail.com>
2017-12-22 21:59:36 -0800

    Add day5 2017

---
+++ Day5-Maze/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Maze
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rData = LoadData("input.txt");
+            var steps = 1;
+            var currentLocation = 0;
+            while (true)
+            {
+                var spot = currentLocation;
+                currentLocation = currentLocation + rData[spot];
+
+                if (currentLocation >= rData.Count || currentLocation < 0)
+                {
+                    Console.WriteLine($"Steps = {steps}");
+                    break;
+                }
+
+                steps++;
+                if (rData[spot] >= 3)
+                {
+                    rData[spot]--;
+                }
+                else
+                {
+                    rData[spot]++;
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+        private static List<int> LoadData(string path)
+        {
+            var rData = new List<int>();
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    rData.Add(int.Parse(s));
+                }
+            }
+
+            return rData;
+        }
+    }
+}
