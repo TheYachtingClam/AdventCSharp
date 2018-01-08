a25ed161c400c58db65aecc1ce3b23a9d031d96f
Nicholas Mayne <nmayne@gmail.com>
2018-01-07 22:31:24 -0800

    structure for first part of 12

---
+++ Day12-DigitalPlumber/MyPrograms.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_DigitalPlumber
+{
+    class MyPrograms
+    {
+        private static Dictionary<int, List<int>> data;
+        private static Dictionary<int, bool> alreadyVisited;
+
+        public List<MyPrograms> ConnectedTo = new List<MyPrograms>();
+
+        public static void SetData(Dictionary<int, List<int>> data)
+        {
+            MyPrograms.data = data;
+        }
+
+        public static List<int> ProgramsAbleToCommunicateTo(int id)
+        {
+            alreadyVisited = new Dictionary<int, bool>();
+            var items = new List<int>();
+            foreach (var myProg in data)
+            {
+                if(CanProgramSeeId(myProg.Key, id))
+                {
+                    items.Add(myProg.Key);
+                }
+            }
+            return items;
+        }
+
+        private static bool CanProgramSeeId(int prog, int id)
+        {
+            if (prog == id)
+            {
+                return true;
+            }
+            else
+            {
+                foreach(var p in data[prog])
+                {
+                    if(alreadyVisited.ContainsKey(p))
+                    {
+                        return alreadyVisited[p];
+                    }
+
+                    if(p == prog)
+                    {
+                        alreadyVisited[prog] = false;
+                        return false;
+                    }
+
+                    var val = CanProgramSeeId(p, id);
+                    alreadyVisited[p] = val;
+                    return val;
+                }
+                alreadyVisited[prog] = false;
+                return false;
+            }
+        }
+    }
+}
---
+++ Day12-DigitalPlumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_DigitalPlumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = LoadData("input.txt");
+
+            MyPrograms.SetData(data);
+
+            var frank = MyPrograms.ProgramsAbleToCommunicateTo(0);
+            Console.WriteLine($"the following programs can see 0, {string.Join(",", frank)}");
+            Console.ReadKey();
+        }
+
+        
+
+        private static Dictionary<int, List<int>> LoadData(string path)
+        {
+            var rData = new Dictionary<int, List<int>>();
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    var id = int.Parse(s.Split('<')[0].Trim());
+                    var ids = s.Split('>')[1].Trim().Split(',');
+                    var listIds = new List<int>();
+                    foreach(var linkedId in ids)
+                    {
+                        listIds.Add(int.Parse(linkedId.Trim()));
+                    }
+                    rData[id] = listIds;
+                }
+            }
+
+            return rData;
+        }
+    }
+}
