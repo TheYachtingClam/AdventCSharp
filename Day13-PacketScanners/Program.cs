608267a4a341f761e0d970ee63837a0321a35d29
Nicholas Mayne <nmayne@gmail.com>
2018-01-09 02:23:29 -0800

    finish second step on day13

--- Day13-PacketScanners/Program.cs
+++ Day13-PacketScanners/Program.cs
@@ -12,7 +12,36 @@ namespace Day13_PacketScanners
     {
         static void Main(string[] args)
         {
-            var data = LoadData("test.txt");
+            var data = LoadData("input.txt");
+            var frank = FirewallCost(data, 4);
+            var pathCost = 0;
+            for(int i = 0;true;++i)
+            {
+                pathCost = FirewallCost(data, i);
+                if (pathCost == 0)
+                {
+                    Console.WriteLine($"skip time for 0 path cost is {i}");
+                    break;
+                }
+            }
+            
+
+            Console.WriteLine($"cost of firewal traversal is {pathCost}");
+            Console.ReadKey();
+        }
+
+        public static int FirewallCost(List<Tuple<int, int>> firewall, int skip)
+        {
+            var cost = 0;
+            foreach (var d in firewall)
+            {
+                if ((d.Item1 + skip) % (2 * (d.Item2 - 1)) == 0)
+                {
+                    //cost += d.Item1 * d.Item2;
+                    cost += 1;
+                }
+            }
+            return cost;
         }
 
         private static List<Tuple<int, int>> LoadData(string path)
