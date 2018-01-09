5e9e3649cbf52c2028b1f5d6474729bdddadc68e
Nicholas Mayne <nmayne@gmail.com>
2018-01-09 02:29:35 -0800

    scaffolding for day14

---
+++ Day14-DiskDefrag/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_DiskDefrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = LoadData("input.txt");
+        }
+    }
+}
