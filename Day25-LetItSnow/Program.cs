8033496360c2ac8552c129b64feb70bfd6d665b4
Nicholas Mayne <nmayne@gmail.com>
2018-07-15 00:56:27 -0700

    finish 2015

---
+++ Day25-LetItSnow/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25_LetItSnow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long startingPos = 20151125;
+
+            var val = GetValue(3019, 3010, startingPos);
+
+            Console.WriteLine($"newVal = {val}");
+            Console.ReadLine();
+        }
+
+        static long AdjustFunction(long input)
+        {
+            return (input * 252533) % 33554393;
+        } 
+
+        static long GetValue(int column, int row, long startingPosition)
+        {
+            var rowBaseNumber = row += (column - 1);
+            var startRowIncrement = 0;
+            for(int i = 1;i < rowBaseNumber;++i)
+            {
+                startRowIncrement += rowBaseNumber - i;
+            }
+             var totalIncs = startRowIncrement + column - 1;
+            
+
+            long retVal = startingPosition;
+            for(int j = 0;j < totalIncs;++j)
+            {
+                retVal = AdjustFunction(retVal);
+            }
+            return retVal;
+        }
+    }
+}
