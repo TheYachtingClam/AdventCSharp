768a28d90b38821bbe37c5a1f99cc1562178c23b
Nicholas Mayne <nmayne@gmail.com>
2018-07-30 13:41:54 -0700

    Finished Day2 part A and B

---
+++ Day2-BathroomSecurity/Keypad.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_BathroomSecurity
+{
+    public class Keypad
+    {
+        public int CurrentState { get; private set; } = 5;
+
+        public int Move(Directions dir)
+        {
+            switch (dir)
+            {
+                case Directions.Up:
+                    if (CurrentState == 3)
+                    {
+                        CurrentState = 1;
+                    }
+                    else if (CurrentState == 6)
+                    {
+                        CurrentState = 2;
+                    }
+                    else if (CurrentState == 7)
+                    {
+                        CurrentState = 3;
+                    }
+                    else if (CurrentState == 8)
+                    {
+                        CurrentState = 4;
+                    }
+                    else if (CurrentState == 10)
+                    {
+                        CurrentState = 6;
+                    }
+                    else if (CurrentState == 11)
+                    {
+                        CurrentState = 7;
+                    }
+                    else if (CurrentState == 12)
+                    {
+                        CurrentState = 8;
+                    }
+                    else if (CurrentState == 13)
+                    {
+                        CurrentState = 11;
+                    }
+                    return CurrentState;
+                case Directions.Down:
+                    if (CurrentState == 1)
+                    {
+                        CurrentState = 3;
+                    }
+                    else if (CurrentState == 2)
+                    {
+                        CurrentState = 6;
+                    }
+                    else if (CurrentState == 3)
+                    {
+                        CurrentState = 7;
+                    }
+                    else if (CurrentState == 4)
+                    {
+                        CurrentState = 8;
+                    }
+                    else if (CurrentState == 6)
+                    {
+                        CurrentState = 10;
+                    }
+                    else if (CurrentState == 7)
+                    {
+                        CurrentState = 11;
+                    }
+                    else if (CurrentState == 8)
+                    {
+                        CurrentState = 12;
+                    }
+                    else if (CurrentState == 11)
+                    {
+                        CurrentState = 13;
+                    }
+                    return CurrentState;
+                case Directions.Left:
+                    if (CurrentState != 1 && CurrentState != 2 && CurrentState != 5 && CurrentState != 10 && CurrentState != 13)
+                    {
+                        CurrentState--;
+                    }
+                    return CurrentState;
+                case Directions.Right:
+                    if (CurrentState != 1 && CurrentState != 4 && CurrentState != 9 && CurrentState != 12 && CurrentState != 13)
+                    {
+                        CurrentState++;
+                    }
+                    return CurrentState;
+                default:
+                    throw new Exception("aaaargh");
+            }
+        }
+
+        public int Move(List<Directions> dir)
+        {
+            var retVal = 0;
+            foreach (var d in dir)
+            {
+                retVal = Move(d);
+            }
+            return retVal;
+        }
+    }
+}
---
+++ Day2-BathroomSecurity/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_BathroomSecurity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rData = LoadData("input.txt");
+
+            var keyboard = new Keypad();
+
+            foreach (var c in rData)
+            {
+                var finalKey = keyboard.Move(c);
+                Console.Write(finalKey + ",");
+            }
+            Console.ReadKey();
+        }
+
+        private static List<List<Directions>> LoadData(string path)
+        {
+            var rData = new List<List<Directions>>();
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    var directions = new List<Directions>();
+                    foreach (var dir in s)
+                    {
+                        switch(dir)
+                        {
+                            case 'U':
+                                directions.Add(Directions.Up);
+                                break;
+                            case 'D':
+                                directions.Add(Directions.Down);
+                                break;
+                            case 'L':
+                                directions.Add(Directions.Left);
+                                break;
+                            case 'R':
+                                directions.Add(Directions.Right);
+                                break;
+                        }
+                    }
+                    rData.Add(directions);
+                }
+            }
+
+            return rData;
+        }
+    }
+}
